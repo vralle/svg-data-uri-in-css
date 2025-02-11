74704831612a22bd5b3297440539256c422b3747
vralle <vitaliy.ralle@gmail.com>
2025-02-11 10:39:00 +0300

    Init

---
+++ webpack.config.mjs
@@ -0,0 +1,120 @@
+/**
+ * Webpack configuration
+ */
+
+import { Buffer } from "node:buffer";
+import { dirname, join } from "node:path";
+import { env } from "node:process";
+import url, { URL } from "node:url";
+
+// Plugins
+import HtmlBundlerPlugin from "html-bundler-webpack-plugin";
+
+// Tools
+import svgToMiniDataURI from "mini-svg-data-uri";
+
+// Project configuration
+const __filename = url.fileURLToPath(new URL(import.meta.url));
+const __dirname = dirname(__filename);
+
+const projectPath = __dirname;
+const srcPath = join(projectPath, "src");
+const outPath = join(projectPath, "dist");
+const isProduction = () => env.NODE_ENV === "production";
+
+/**
+ * @typedef {import('webpack').Configuration} WebpackCfg
+ * @typedef {import('webpack-dev-server').Configuration} DevServerCfg
+ * @typedef {import('webpack').Module} Module
+ */
+
+/**
+ * @type {WebpackCfg&DevServerCfg}
+ */
+const webpackConfig = {
+  mode: isProduction() ? "production" : "development",
+  cache: false,
+  output: {
+    clean: true,
+    hashDigestLength: 9,
+  },
+  resolve: {
+    alias: {
+      "@src": srcPath,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/i,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 0,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition(source, { filename }) {
+            console.log("dataUrlCondition: ", filename);
+            return Buffer.byteLength(source) <= 3 * 1024; // =maxSize: 3kb
+          },
+        },
+        generator: {
+          dataUrl: (source, { filename }) => {
+            console.log("dataUrl: ", filename);
+            return svgToMiniDataURI(source.toString());
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlBundlerPlugin({
+      entry: {
+        index: {
+          import: join(srcPath, "views", "index.html"),
+        },
+      },
+      css: {
+        test: /\.css/i,
+        filename: "[name].[contenthash:9].css",
+      },
+      minify: false,
+      preprocessor: false,
+    }),
+  ],
+  optimization: {
+    minimize: false,
+  },
+  devtool: isProduction() ? false : "inline-cheap-source-map",
+  devServer: {
+    compress: true,
+    static: {
+      directory: outPath,
+      publicPath: "",
+    },
+    watchFiles: ["src/**/*.{scss,html}"],
+  },
+  watchOptions: {
+    poll: true,
+    ignored: ["**/node_modules/**"],
+  },
+  stats: {
+    loggingDebug: ["css-loader"],
+    errorDetails: true,
+    children: true,
+    colors: true,
+  },
+  infrastructureLogging: {
+    level: "verbose",
+  },
+};
+
+export default webpackConfig;
